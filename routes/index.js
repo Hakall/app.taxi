7738668ce17534ebf57b7084dfe36a4499e0522f
Hakall <aboukine@gmail.com>
2015-12-14 20:02:02 +0100

    Récupération des taxis, il faut mettre en forme la vue des résultats

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var request = require("request");
+var fs = require('fs');
+
+var services = {},
+    services_path = process.cwd() + '/services';
+
+fs.readdirSync(services_path).forEach(function(file) {
+    if (file.indexOf('.js') != -1) {
+        services[file.split('.')[0]] = require(services_path + '/' + file)
+    }
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', {
+        title: 'Taxifinder'
+    });
 });
 
+router.get('/taxis', function(req, res, next) {
+    var resultats;
+    request({
+        uri: "https://dev.api.taxi/taxis/?lon=2.3488000&lat=48.8534100",
+        method: "GET",
+        headers: {
+            'accept': "application/json",
+            "x-version": 2,
+            "x-api-key": "46f06ed1-0124-4edc-9283-0df69a604ef4"
+        }
+    }, function(error, response, body) {
+        //a faire si besoin de parser plus finement
+        /*services.le_taxi_parser.parse(resultats);*/
+        res.render('results', JSON.parse(body));
+    });
 
+});
 
 
 module.exports = router;
---
+++ services/le_taxi_parser.js
@@ -0,0 +1,3 @@
+exports.parse=function(data){
+	console.log(data.data);
+}
