f0217e54921c003ec984a11801c159c78368a7e6
Hakall <aboukine@gmail.com>
2015-12-15 11:18:10 +0100

    Listing ok, hails non fonctionnel

--- routes/index.js
+++ routes/index.js
@@ -37,5 +37,36 @@ router.get('/taxis', function(req, res, next) {
 
 });
 
+router.get('/hails/:idTaxi', function(req, res, next) {
+    console.log(req.params.idTaxi);
+    var resultat;
+    request({
+        uri: "https://dev.api.taxi/hails/",
+        method: "POST",
+        headers: {
+            'accept': "application/json",
+            "x-version": 2,
+            "x-api-key": "46f06ed1-0124-4edc-9283-0df69a604ef4"
+        },
+        form: {
+            payload: {
+                "data": [{
+                    "customer_address": "string",
+                    "customer_id": "string",
+                    "customer_lat": 0,
+                    "customer_lon": 0,
+                    "customer_phone_number": "string",
+                    "operateur": "string",
+                    "taxi_id": req.params.idTaxi
+                }]
+            }
+        }
+    }, function(error, response, body) {
+        //a faire si besoin de parser plus finement
+        /*services.le_taxi_parser.parse(resultat);*/
+        console.log(body);
+        /*res.render('results', JSON.parse(body));*/
+    });
+});
 
 module.exports = router;
